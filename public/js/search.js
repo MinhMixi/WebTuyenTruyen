d6ef564ed1cf6ee3bd0d59ecf289f6a17fe5b6ea
MinhMixi <ngotuanminh05082002@gmail.com>
2024-05-02 16:34:59 +0700

    update

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,38 @@
-function search() {
+// function search() {
+//     var keyword = document.getElementById("search-input").value;
+//     var encodedKeyword = encodeURIComponent(keyword);
+//     window.location.href = "TimKiem.html?keyword=" + encodedKeyword;
+//     return false; // Ngăn chặn gửi biểu mẫu
+// }
+
+function searchAndHighlight() {
     var keyword = document.getElementById("search-input").value;
-    var encodedKeyword = encodeURIComponent(keyword);
-    window.location.href = "TimKiem.html?keyword=" + encodedKeyword;
-    return false; // Ngăn chặn gửi biểu mẫu
+    var searchText = new RegExp(keyword, "gi");
+
+    var contentElement = document.getElementById("content");
+    var contentHTML = contentElement.innerHTML;
+    var matches = contentHTML.match(searchText);
+    if (matches) {
+        // Xóa các lớp 'highlighted' trước khi làm mới
+        contentElement.innerHTML = contentHTML.replace(/<span class="highlighted">(.*?)<\/span>/g, '$1');
+
+        // Tìm và làm nổi bật tất cả các trùng khớp của từ khóa tìm kiếm
+        contentElement.innerHTML = contentElement.innerHTML.replace(searchText, '<span class="highlighted">$&</span>');
+
+        // Cuộn đến từ khóa tìm kiếm đầu tiên
+        var firstMatch = document.querySelector('.highlighted');
+        if (firstMatch) {
+            firstMatch.scrollIntoView();
+        }
+    }
 }
+
+// Thêm sự kiện click cho nút tìm kiếm
+document.getElementById("search-button").addEventListener("click", searchAndHighlight);
+
+// Thêm sự kiện keypress cho ô nhập văn bản để xử lý khi người dùng nhấn Enter
+document.getElementById("search-input").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        searchAndHighlight();
+    }
+});
